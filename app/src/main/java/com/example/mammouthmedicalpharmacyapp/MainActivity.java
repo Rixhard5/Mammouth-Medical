ca37ee992ee36b13a3dd5df596ad669f71b0c8eb
Richard <127592434+Rixhard5@users.noreply.github.com>
2024-05-11 18:13:26 +0200

    minor warning fix

--- app/src/main/java/com/example/mammouthmedicalpharmacyapp/MainActivity.java
+++ app/src/main/java/com/example/mammouthmedicalpharmacyapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
